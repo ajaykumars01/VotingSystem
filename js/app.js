7d86c19448dd410ef6614d186870c7758176afb9
ajaykumars01 <kumarsahani454@gmail.com>
2026-04-05 16:16:00 +0530

    Final working voting system

---
+++ js/app.js
@@ -0,0 +1,76 @@
+// REGISTER
+function registerUser(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Fill all fields");
+        return false;
+    }
+
+    localStorage.setItem("user", username);
+    localStorage.setItem("pass", password);
+
+    alert("Registration Successful");
+    window.location.href = "vote.html";
+}
+
+// LOGIN
+function loginUser(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let savedUser = localStorage.getItem("user");
+    let savedPass = localStorage.getItem("pass");
+
+    if (username === savedUser && password === savedPass) {
+        alert("Login Successful");
+        window.location.href = "vote.html";
+    } else {
+        alert("Invalid Credentials");
+    }
+}
+
+// VOTE
+function vote(party) {
+    let voted = localStorage.getItem("voted");
+
+    if (voted === "yes") {
+        alert("You already voted!");
+        return;
+    }
+
+    let count = localStorage.getItem(party) || 0;
+    localStorage.setItem(party, parseInt(count) + 1);
+    localStorage.setItem("voted", "yes");
+
+    alert("Vote Successful");
+}
+
+// RESULTS
+function loadResults() {
+    let bjp = localStorage.getItem("bjp") || 0;
+    let brs = localStorage.getItem("brs") || 0;
+    let congress = localStorage.getItem("congress") || 0;
+
+    document.getElementById("bjpCount").innerText = bjp;
+    document.getElementById("brsCount").innerText = brs;
+    document.getElementById("congressCount").innerText = congress;
+
+    let winner = "BJP";
+    let max = bjp;
+
+    if (brs > max) {
+        winner = "BRS";
+        max = brs;
+    }
+    if (congress > max) {
+        winner = "CONGRESS";
+    }
+
+    document.getElementById("winner").innerText = winner;
+}
