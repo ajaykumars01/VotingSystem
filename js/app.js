d341afc20c4a15614a9f9ba68b2dbcb9c30e5970
ajaykumars01 <kumarsahani454@gmail.com>
2026-04-05 17:24:05 +0530

    Fix login issue

--- js/app.js
+++ js/app.js
@@ -1,30 +1,16 @@
-function registerUser(e) {
+function loginUser(e) {
     e.preventDefault();
 
-    let username = document.getElementById("username").value.trim();
-    let password = document.getElementById("password").value.trim();
-
-    if (username === "" || password === "") {
-        alert("Username and Password cannot be empty!");
-        return false;
-    }
-
-    if (username.length < 3 || password.length < 3) {
-        alert("Minimum 3 characters required!");
-        return false;
-    }
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
     let savedUser = localStorage.getItem("user");
-    if (username === savedUser) {
-        alert("Username already exists!");
-        return false;
+    let savedPass = localStorage.getItem("pass");
+
+    if (username === savedUser && password === savedPass) {
+        alert("Login Successful");
+        window.location.href = "vote.html";
+    } else {
+        alert("Invalid Credentials");
     }
-
-    localStorage.setItem("user", username);
-    localStorage.setItem("pass", password);
-
-    alert("Registration Successful! Please login.");
-
-    // ✅ NOW GO TO LOGIN PAGE
-    window.location.href = "login.html";
 }
