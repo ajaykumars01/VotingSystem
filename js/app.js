9c316429bfdd962cedac5e5847b240babc5111ac
ajaykumars01 <kumarsahani454@gmail.com>
2026-04-05 17:32:53 +0530

    Fix empty register page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+// REGISTER
+function registerUser(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if (username === "" || password === "") {
+        alert("Fill all fields!");
+        return false;
+    }
+
+    localStorage.setItem("user", username);
+    localStorage.setItem("pass", password);
+
+    alert("Registration Successful!");
+    window.location.href = "login.html";
+}
+
+// LOGIN
 function loginUser(e) {
     e.preventDefault();
 
@@ -8,9 +28,9 @@ function loginUser(e) {
     let savedPass = localStorage.getItem("pass");
 
     if (username === savedUser && password === savedPass) {
-        alert("Login Successful");
+        alert("Login Successful!");
         window.location.href = "vote.html";
     } else {
-        alert("Invalid Credentials");
+        alert("Invalid Credentials!");
     }
 }
