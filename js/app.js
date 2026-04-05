35877fd7e7f9ec8a36304b22806d95719511999f
ajaykumars01 <kumarsahani454@gmail.com>
2026-04-05 16:30:01 +0530

    Fix logic

--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@
 function registerUser(e) {
     e.preventDefault();
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
 
     if (username === "" || password === "") {
-        alert("Fill all fields");
+        alert("Username and Password cannot be empty!");
+        return false;
+    }
+
+    if (username.length < 3 || password.length < 3) {
+        alert("Minimum 3 characters required!");
+        return false;
+    }
+
+    let savedUser = localStorage.getItem("user");
+    if (username === savedUser) {
+        alert("Username already exists!");
         return false;
     }
 
